7df54373981f1db3769cd3b0a1d50ef9398ccfb8
renlov <a.pimenov95@mail.ru>
2022-05-18 18:52:17 +0300

    added inline button

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -64,22 +64,22 @@ suspend fun main() {
 
         onUnhandledCommand {
 
-            onCommand("start", initialFilter = { it.chat is PrivateChat && startedChats.add(it.chat) }){
-                reply(it, "Hello, this is a gray department bot.\n" +
-                                "In this chat you can add apps, search + correct data and find all apps\n\n" +
-                                "You have this commands:\n" +
-                                "/apps - find all apps\n" +
-                                "/search - to find app information and correct data\n" +
-                                "/put - to add app in database\n\n" +
-                                "if bot asleep, open link in browser to wake up bot\n" +
-                                "https://telegrambotgrey.herokuapp.com\n" +
-                                "АНЯ, если что-то сломала, не трогай больше ничего и напиши нам!"
-                    )
-                println(it.chat)
-                startedChats.forEach {chat ->
-                    println(chat.id)
-                }
-            }
+//            onCommand("start", initialFilter = { it.chat is PrivateChat && startedChats.add(it.chat) }){
+//                reply(it, "Hello, this is a gray department bot.\n" +
+//                                "In this chat you can add apps, search + correct data and find all apps\n\n" +
+//                                "You have this commands:\n" +
+//                                "/apps - find all apps\n" +
+//                                "/search - to find app information and correct data\n" +
+//                                "/put - to add app in database\n\n" +
+//                                "if bot asleep, open link in browser to wake up bot\n" +
+//                                "https://telegrambotgrey.herokuapp.com\n" +
+//                                "АНЯ, если что-то сломала, не трогай больше ничего и напиши нам!"
+//                    )
+//                println(it.chat)
+//                startedChats.forEach {chat ->
+//                    println(chat.id)
+//                }
+//            }
             onCommand("info") {
                 sendMessage(
                     it.chat, "Hello, this is a gray department bot.\n" +
